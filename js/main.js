40d4ad175f05cb52f6a5e2bb1e7666f6baafa106
rita <ritacarmenortiz@gmail.com>
2017-01-30 03:49:45 -0500

    agregando js y mas

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(main);
+var contador = 1;
+function main(){
+  $(".menu-bar").click(function(){
+  //  $("nav").toggle();
+  if(contador==1){
+    $("nav").animate({
+      left: "26px"
+    });
+    contador = 0;
+  }else {
+    contador = 1;
+    $("nav").animate({
+      left: "-100%"
+    });
+  }
+  })
+}
